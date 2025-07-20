594c7d507470a0a5ff5716a2d9811a1f28a0b93b
Joshua01sancho <musukambalejoshua@gmail.com>
2025-07-21 00:14:15 +0200

    Logic Changed

--- backend/apps/voters/management/commands/clear_blockchain_addresses.py
+++ backend/apps/voters/management/commands/clear_blockchain_addresses.py
@@ -3,7 +3,7 @@ from apps.voters.models import Voter
 
 class Command(BaseCommand):
     help = 'Clear blockchain addresses and private keys for all users.'
-
+ 
     def handle(self, *args, **options):
         updated = Voter.objects.update(blockchain_address=None, blockchain_private_key=None)
         self.stdout.write(self.style.SUCCESS(f'Cleared blockchain addresses and private keys for {updated} users.')) 
